1b51428bd680dfd12718f6ecf9608e6551d56ee6
deangallant <deangallant@student.mitt.ca>
2021-05-26 15:02:23 -0500

    100 changes

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+
+
+
 const currentLocation = (position) => {
-  const {latitude, longitude} = position.coords;
-  console.log(longitude, latitude);
-  mapCurrentLocation(longitude, latitude);
+  //console.log(longitude, latitude);
+  document.querySelector('.lat').innerHTML = position.coords.latitude;
+  document.querySelector('.lon').innerHTML = position.coords.longitude;
+  
+  mapCurrentLocation();
 }
 
 const error = () => {
@@ -16,17 +21,55 @@ const currentLocationMarker = (map, longitude, latitude) => {
   console.log(marker1);
 }
 
-const mapCurrentLocation = (longitude, latitude) => {
+const mapCurrentLocation = () => {
+  
+  const lat = document.querySelector('.lat').innerHTML;
+  const lon = document.querySelector('.lon').innerHTML;
+  console.log(lat, lon)
   mapboxgl.accessToken = 'pk.eyJ1IjoibWF5dXJwYXRlbDc4NjQ1IiwiYSI6ImNrcDVrYjh0cjF5azAyb3RhbjBnOWN6ajIifQ.MS0bHsUsb-hMZ1dABPcqHQ';
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [longitude, latitude], 
+    center: [lon, lat], 
     zoom: 13
   });
-  currentLocationMarker(map, longitude, latitude);
+
+  currentLocationMarker(map, lon, lat);
+  
 }
+
+
 
 (function getUserLocation() {
   navigator.geolocation.getCurrentPosition(currentLocation, error);
 }());
+
+
+
+const mapSearch = async (userQuery) => {
+  const lon = (document.querySelector('.lon').innerHTML);
+  const lat = (document.querySelector('.lat').innerHTML);
+  console.log(userQuery)
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${userQuery}.json?proximity=${[lon, lat]}&access_token=pk.eyJ1IjoibWF5dXJwYXRlbDc4NjQ1IiwiYSI6ImNrcDVrYjh0cjF5azAyb3RhbjBnOWN6ajIifQ.MS0bHsUsb-hMZ1dABPcqHQ`
+  console.log(url)
+  const userSearch = await fetch(url)
+  const response = await userSearch.json()
+  console.log(response)
+  //response 
+  //pass response to render
+}
+
+
+const inputForm = document.querySelector('.form');
+inputForm.addEventListener('keydown', (e) => {
+  if(e.keyCode === 13){
+    e.preventDefault()
+    e.target.value = ''
+  }
+  mapSearch(e.target.value);
+});
+
+// search text from input field html
+// autocomplete
+// 
+//   /geocoding/v5/{mapbox.places}/{search_text}/autocomplete=true.json
